48af2db276f368b354448217a74210d3b3c353a1
unknown <denny.fict@gmail.com>
2021-06-12 22:32:09 +0300

    'updated'

--- app/backend/functions.py
+++ app/backend/functions.py
@@ -5,7 +5,7 @@ from objects import *
 
 
 def book_pages(s):
-    return pd.DataFrame(s.str.extract(r'(\d+)'), columns=['book_pages']).fillna(value=medians_dict[s.name]).astype('int')
+    return pd.DataFrame(s.str.extract(r'(\d+)'), columns=['book_pages']).fillna(value=0).astype('int')
 
 
 def book_image_area(s):
@@ -16,19 +16,19 @@ def book_image_area(s):
             height, width = io.imread(url).shape[:2]
             arr.append(height * width)
         except Exception:
-            arr.append(medians_dict['book_image_area'])
+            arr.append(0)
 
-    return pd.DataFrame({'book_image_area':arr})
+    return pd.DataFrame({'book_image_area': arr})
 
 
 def book_review_count(s):
-    return pd.DataFrame(s.fillna(value=medians_dict[s.name]))
+    return pd.DataFrame(s.fillna(value=0))
 
 
 def book_genre_count(s):
     top_genres = genres_data.sort_values(by='count', ascending=False)['genre'].head(30).values
     return pd.DataFrame(s.map(lambda x: sum([1 for genre in np.unique(x.split('|')) if genre in top_genres])
-                              if pd.notna(x) else medians_dict['book_genre_count'])).rename(columns={s.name:'book_genre_count'})
+                              if pd.notna(x) else 0)).rename(columns={s.name: 'book_genre_count'})
 
 
 def authors_info(s):
@@ -41,9 +41,4 @@ def authors_info(s):
         info_arr.append(
             np.array(arr if len(arr) else [np.nan] * 3).mean(axis=0).flatten() if len(arr) else [np.nan] * 3)
     return pd.DataFrame(info_arr, columns=['author_work_count', 'author_average_rating', 'author_review_count']) \
-        .fillna(value={'author_work_count': medians_dict['author_work_count'],
-                       'author_average_rating': medians_dict['author_average_rating'],
-                       'author_review_count': medians_dict['author_review_count']})
-
-
-
+        .fillna(value={'author_work_count': 0, 'author_average_rating': 0, 'author_review_count': 0})
